d9eff99680ab823dac0c17248a3050063da06dfe
neoh0912 <neoh@marsta.net>
2026-04-30 09:15:43 +0200

    u

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-LINUX_BUILD_DIR=$PWD/build/linux
-INITRAMFS_DIR=$PWD/build/initramfs.cpio.gz
+BZIMAGE=$PWD/bzImage
+INITRAMFS=$PWD/.build/initramfs.cpio.gz
 
 qemu-system-x86_64 \
 -m 512 \
--kernel $LINUX_BUILD_DIR/arch/x86/boot/bzImage \
+-kernel $BZIMAGE \
 -nographic \
 -append "earlyprintk=serial,ttyS0 console=ttyS0" \
---initrd $INITRAMFS_DIR
+--initrd $INITRAMFS
